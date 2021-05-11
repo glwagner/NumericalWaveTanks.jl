88dd8bbfe5c0dc1c08ac4f518fb9b759a42837f0
Gregory Wagner <wagner.greg@gmail.com>
2021-05-11 08:44:09 -0800

    Updates visualization script

--- sandbox/visualize.jl
+++ sandbox/visualize.jl
@@ -18,11 +18,13 @@ grid = RegularRectilinearGrid(size = (Nx, Ny, Nz), halo = (3, 3, 3),
                               topology = (Periodic, Bounded, Bounded))
 
 xw, yw, zw = nodes((Center, Center, Face), grid)
+xu, yu, zu = nodes((Face, Center, Center), grid)
 
 iterations = parse.(Int, keys(yz_file["timeseries/t"]))
 
 anim = @animate for (i, iter) in enumerate(iterations)
     wyz = yz_file["timeseries/w/$iter"][1, :, :]
+    uyz = yz_file["timeseries/u/$iter"][1, :, :]
     t = yz_file["timeseries/t/$iter"]
 
     wmax = maximum(abs, wyz)
@@ -30,18 +32,35 @@ anim = @animate for (i, iter) in enumerate(iterations)
     wlevels = range(-wlim, stop=wlim, length=30)
     wlim < wmax && (wlevels = vcat([-wmax], wlevels, [wmax]))
 
-    wyz_title = @sprintf("w(y, z, t) (m s⁻¹) at t = %s ", prettytime(t))
-    @show wyz_title
+    umax = maximum(abs, uyz)
+    ulim = umax / 2
+    ulevels = range(-ulim, stop=ulim, length=30)
+    ulim < umax && (ulevels = vcat([-umax], ulevels, [umax]))
 
-    contourf(yw, zw, wyz';
-             linewidth = 0,
-             aspectratio = :equal,
-             xlims = (0, grid.Ly),
-             ylims = (-grid.Lz, 0),
-             color = :balance,
-             levels = wlevels,
-             clims = (-wlim, wlim),
-             title = wyz_title)
+    w_title = @sprintf("w(y, z, t) (m s⁻¹) at t = %s ", prettytime(t))
+    u_title = @sprintf("u(y, z, t) (m s⁻¹) at t = %s ", prettytime(t))
+
+    @show w_title
+
+    wplot = contourf(yw, zw, wyz';
+                     linewidth = 0,
+                     aspectratio = :equal,
+                     xlims = (0, grid.Ly),
+                     ylims = (-grid.Lz, 0),
+                     color = :balance,
+                     levels = wlevels,
+                     clims = (-wlim, wlim))
+
+    uplot = contourf(yu, zu, uyz';
+                     linewidth = 0,
+                     aspectratio = :equal,
+                     xlims = (0, grid.Ly),
+                     ylims = (-grid.Lz, 0),
+                     color = :balance,
+                     levels = ulevels,
+                     clims = (-ulim, ulim))
+
+    plot(wplot, uplot, layout=(2, 1), title=[w_title u_title])
 end
 
 close(yz_file)
