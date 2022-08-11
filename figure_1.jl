7b3ec24e20e1a915b87ebe04dcb82a2e76e8087b
Gregory Wagner <wagner.greg@gmail.com>
2022-08-11 10:48:30 -0600

    Update figure analysis

--- figure_1.jl
+++ figure_1.jl
@@ -1,36 +1,29 @@
-#=
 using GLMakie
 using JLD2
 using MAT
 using Oceananigans
 
 dir = "data"
-yz_filename = "increasing_wind_ep10_k30_N512_512_384_L20_20_10_yz_left.jld2"
-statistics_filename = "increasing_wind_ep10_k30_N512_512_384_L20_20_10_statistics.jld2"
-yz_filepath = joinpath(dir, yz_filename)
-=#
+#case = "increasing_wind_ep10_k30_N512_512_384_L20_20_10"
+#case = "increasing_wind_ep27_k30_N384_384_256_L10_10_5"
+case = "increasing_wind_ep28_k30_N384_384_256_L10_10_5"
+#case = "increasing_wind_ep27_k30_N512_512_384_L10_10_5"
+statistics_filename = case * "_statistics.jld2"
+averages_filename   = case * "_averages.jld2"
+
+statistics_filepath = joinpath(dir, statistics_filename)
+averages_filepath   = joinpath(dir, averages_filename)
 
 #####
 ##### Load LIF data
 #####
 
 ramp = "2"
-tlif_filename = string("TRANSVERSE_STAT_RAMP", ramp, "_LIF_final.mat")
-tlif_data = matread(tlif_filename)["STAT_R$ramp"]
-
-# Load LIF data as "concentration"
-c_tlif = tlif_data["LIFa"]
-c_tlif = permutedims(c_tlif, (2, 3, 1)) # puts time in last dimension
-
-# Load time (convert from 2D array to 1D vector)
-t_tlif = tlif_data["time"][:]
-t_tlif = t_tlif .- t_tlif[1]
-Nt_tlif = length(t_tlif)
-
 llif_filename = "LIF_analysis_mean_profiles_ALL_EXPS.mat"
-llif_data = matread(llif_filename)["STAT_R2_EXP1"]
-t_llif = llif_data["time"]
+llif_data = matread(llif_filename)["STAT_R" * ramp * "_EXP2"]
+t_llif = llif_data["time"][:] .- 98.8
 c_llif = llif_data["mean_LIF"]
+z_llif = -llif_data["depth_relative_to_surface"][:] .+ 0.001
 
 #####
 ##### Load simulation data
@@ -61,24 +54,43 @@ end
 stats = compute_timeseries(statistics_filepath)
 t_stats = stats[:t]
 u_max = stats[:u_max]
-u_max_max, n_max = findmax(u_max)
-t_max = t_stats[n_max]
-t_stats = t_stats .- t_max
+u_min = stats[:u_min]
+n_transition = findfirst(i -> u_max[i+1] < u_max[i] - 1e-2, 1:length(u_max)-1)
+t_transition = t_stats[n_transition]
+t_stats = t_stats .- t_transition
+@show t_transition
 
-c_sim = FieldTimeSeries(yz_filepath, "c")
-t_sim = c_sim.times
+ct = FieldTimeSeries(averages_filepath, "c")
+c_sim = interior(ct, 1, 1, :, :)
+z_sim = znodes(Center, ct.grid)
+t_sim = ct.times .- t_transition
 Nt = length(t_sim)
 
 # Figure
-fig = Figure(resolution=(2000, 1200))
+t₀ = -10
+t₁ = 10
+z₀ = -0.04
+fig = Figure(resolution=(1100, 1200))
 colormap = :bilbao
 
-ax_llif = Axis(fig[2, 1])
-heatmap!(ax_llif, c_llif)
-# Scatter plot
-ax_u = Axis(fig[1, 1])
+ax_u    = Axis(fig[1, 1], xlabel="Time relative to turbulent transition (seconds)", ylabel="Maximum surface velocity (m s⁻¹)")
+ax_llif = Axis(fig[2, 1], xlabel="Time relative to turbulent transition (seconds)", ylabel="z (m)")
+ax_sim  = Axis(fig[3, 1], xlabel="Time relative to turbulent transition (seconds)", ylabel="z (m)")
 
-surface_velocity_data = matread(filename)["BIN"]["R$ramp"]
+xlims!(ax_u,    t₀, t₁)
+xlims!(ax_llif, t₀, t₁)
+xlims!(ax_sim,  t₀, t₁)
+
+ylims!(ax_llif, z₀, 0.0)
+ylims!(ax_sim,  z₀, 0.0)
+
+# Heatmaps
+heatmap!(ax_llif, t_llif, z_llif, c_llif; colorrange=(50, 500), colormap)
+heatmap!(ax_sim, t_sim, z_sim, c_sim'; colormap, colorrange=(0, 0.02))
+
+# Scatter plot
+surface_velocity_filename = "Final_SurfVel_per_RAMP.mat"
+surface_velocity_data = matread(surface_velocity_filename)["BIN"]["R$ramp"]
 t_surf = surface_velocity_data["time"][:]
 u_surf = surface_velocity_data["U"][:]
 
@@ -87,12 +99,16 @@ u_surf_max_max, n_max = findmax(u_surf_max)
 t_surf_max = t_surf[n_max]
 t_surf = t_surf .- t_surf_max
 
-scatter!(ax_u, t_surf, u_surf, marker=:circle)
-scatter!(ax_u, t_stats, u_max)
+scatter!(ax_u, t_surf, u_surf, marker=:utriangle, markersize=15, color=(:blue, 0.6), label="Lab (feature tracking)")
+scatter!(ax_u, t_stats, u_max, markersize=10, color=(:red, 0.6), label="max(u), simulation")
+scatter!(ax_u, t_stats, u_min, markersize=10, color=(:orange, 0.6), label="min(u), simulation")
+axislegend(ax_u, position=:rb)
 
-tn_tlif = @tlift t_tlif[$n] - 19.5 #- t_surf_max
+#=
+tn_tlif = @lift t_tlif[$n] - 19.5 #- t_surf_max
 vlines!(ax_u, tn_tlif, linestyle=:solid, linewidth=3, color=(:black, 0.6))
 vlines!(ax_u, tn_sim, linestyle=:dash, linewidth=3, color=(:red, 0.8))
+=#
 
 display(fig)
 
--- figure_2.jl
+++ figure_2.jl
@@ -1,14 +1,19 @@
 #=
-using GLMakie
+#using GLMakie
 using JLD2
 using MAT
 using Oceananigans
 
+set_theme!(Theme(fontsize=24))
+
 dir = "data"
-yz_filename = "increasing_wind_ep10_k30_N512_512_384_L20_20_10_yz_left.jld2"
-statistics_filename = "increasing_wind_ep10_k30_N512_512_384_L20_20_10_statistics.jld2"
+case = "increasing_wind_ep27_k30_N384_384_256_L10_10_5"
+
+yz_filename         = case * "_yz_left.jld2"
+statistics_filename = case * "_statistics.jld2"
+
 yz_filepath = joinpath(dir, yz_filename)
-=#
+statistics_filepath = joinpath(dir, statistics_filename)
 
 #####
 ##### Load LIF data
@@ -26,11 +31,6 @@ c_tlif = permutedims(c_tlif, (2, 3, 1)) # puts time in last dimension
 t_tlif = tlif_data["time"][:]
 t_tlif = t_tlif .- t_tlif[1]
 Nt_tlif = length(t_tlif)
-
-llif_filename = "LIF_analysis_mean_profiles_ALL_EXPS.mat"
-llif_data = matread(llif_filename)["STAT_R2_EXP1"]
-t_llif = llif_data["time"]
-c_llif = llif_data["mean_LIF"]
 
 #####
 ##### Load simulation data
@@ -61,25 +61,26 @@ end
 stats = compute_timeseries(statistics_filepath)
 t_stats = stats[:t]
 u_max = stats[:u_max]
-u_max_max, n_max = findmax(u_max)
-t_max = t_stats[n_max]
-t_stats = t_stats .- t_max
+n_transition = findfirst(i -> u_max[i+1] < u_max[i], 1:length(u_max)-1)
+t_transition = t_stats[n_transition]
+t_stats = t_stats .- t_transition
 
 c_sim = FieldTimeSeries(yz_filepath, "c")
 t_sim = c_sim.times
 Nt = length(t_sim)
+=#
 
 # Figure
-fig = Figure(resolution=(2000, 1200))
+fig = Figure(resolution=(2000, 800))
 colormap = :bilbao
 
-ax_llif = Axis(fig[2, 1])
-heatmap!(ax_llif, c_llif)
-
 # LIF data heatmap
-ax_tlif = Axis(fig[6, 1])
-tlif_slider = Slider(fig[7, 1], range=1:Nt_tlif, startvalue=151)
-n = tlif_slider.value
+# ax_u    = Axis(fig[1, 1], xlabel="Time relative to turbulent transition (seconds)", ylabel="Maximum surface velocity (m s⁻¹)")
+ax_sim  = Axis(fig[1, 1], xlabel="Transverse distance (m)", ylabel="Vertical distance (m)")
+ax_tlif = Axis(fig[2, 1], xlabel="Transverse distance (m)", ylabel="Vertical distance (m)")
+
+#sim_slider  = Slider(fig[3, 1], range=1:Nt, startvalue=n_transition)
+#tlif_slider = Slider(fig[5, 1], range=1:Nt_tlif, startvalue=151)
 
 j1 = 1200
 j2 = 1600
@@ -88,29 +89,41 @@ x_tlif = tlif_data["X_transverse_m"][:]
 z_tlif = tlif_data["Z_transverse_m"][j1:j2]
 z_tlif = z_tlif .- maximum(z_tlif)
 
-cn_tlif = @tlift rotr90(view(c_tlif, :, :, $n))[:, j1:j2]
+xlims!(ax_sim, 0, maximum(x_tlif))
+ylims!(ax_sim, minimum(z_tlif), 0)
+
+#n = tlif_slider.value
+#cn_tlif = @lift rotr90(view(c_tlif, :, :, $n))[:, j1:j2]
+#tn_tlif = @lift t_tlif[$n] - 19.5 #- t_surf_max
+
+n = 151
+cn_tlif = rotr90(view(c_tlif, :, :, n))[:, j1:j2]
+tn_tlif = t_tlif[n] - 19.5 #- t_surf_max
 heatmap!(ax_tlif, x_tlif, z_tlif, cn_tlif; colorrange=(50, 2500), colormap)
 
 # Simulation heatmap
-ax_sim = Axis(fig[4, 1])
-sim_slider = Slider(fig[5, 1], range=1:Nt, startvalue=n_max)
-n = sim_slider.value
-
 grid = c_sim.grid
 Nx, Ny, Nz = size(grid)
-Nh = findfirst(x -> x > maximum(x_tlif), xnodes(Face, grid))
-@show Nh
-Nv = 240
-tn_sim = @tlift t_sim[$n] - t_max
-cn_sim = @tlift interior(c_sim[$n], 1, 1:Nh, Nv:Nz)
+#n = sim_slider.value
+#tn_sim = @lift t_sim[$n] - t_transition
+#cn_sim = @lift begin
+#    c = interior(c_sim[$n], 1, :, :)
+#    vcat(c, c)
+#end
+
+n = 64
+tn_sim = t_sim[n] - t_transition
+cn_sim = interior(c_sim[n], 1, :, :)
+cn_sim = vcat(cn_sim, cn_sim)
 
 x_sim, y_sim, z_sim = nodes(c_sim)
-heatmap!(ax_sim, y_sim[1:Nh], z_sim[Nv:Nz], cn_sim; colorrange=(0.01, 0.065), colormap)
+y_sim = range(0.0, stop=2y_sim[Ny], length=2Ny)
+
+heatmap!(ax_sim, y_sim, z_sim, cn_sim; colorrange=(0.0001, 0.1), colormap)
 
 # Scatter plot
-ax_u = Axis(fig[1, 1])
-
-surface_velocity_data = matread(filename)["BIN"]["R$ramp"]
+surface_velocity_filename = "Final_SurfVel_per_RAMP.mat"
+surface_velocity_data = matread(surface_velocity_filename)["BIN"]["R$ramp"]
 t_surf = surface_velocity_data["time"][:]
 u_surf = surface_velocity_data["U"][:]
 
@@ -119,15 +132,10 @@ u_surf_max_max, n_max = findmax(u_surf_max)
 t_surf_max = t_surf[n_max]
 t_surf = t_surf .- t_surf_max
 
-scatter!(ax_u, t_surf, u_surf, marker=:circle)
-scatter!(ax_u, t_stats, u_max)
-
-tn_tlif = @tlift t_tlif[$n] - 19.5 #- t_surf_max
-vlines!(ax_u, tn_tlif, linestyle=:solid, linewidth=3, color=(:black, 0.6))
-vlines!(ax_u, tn_sim, linestyle=:dash, linewidth=3, color=(:red, 0.8))
-
-# title = @tlift string("Langmuir instability at t = ", prettytime(t[$n]))
-# Label(fig[1, 1:2], title, tellwidth=false)
+# scatter!(ax_u, t_surf, u_surf, marker=:circle)
+# scatter!(ax_u, t_stats, u_max)
+# vlines!(ax_u, tn_tlif, linestyle=:solid, linewidth=3, color=(:black, 0.6))
+# vlines!(ax_u, tn_sim, linestyle=:dash, linewidth=3, color=(:red, 0.8))
 
 display(fig)
 
