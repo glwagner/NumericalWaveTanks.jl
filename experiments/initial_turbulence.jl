c8f72d98b7cdc51939f1767a5ac084345f55bce1
Gregory Wagner <wagner.greg@gmail.com>
2023-06-17 14:12:32 -0600

    Add a script to compute decaying turbulence embedded in mean shear

---
+++ experiments/initial_turbulence.jl
@@ -0,0 +1,169 @@
+using CUDA
+using Random
+using Statistics
+using SpecialFunctions
+using OrderedCollections
+using JLD2
+using Oceananigans
+using Oceananigans: fields
+using Oceananigans.Forcings: regularize_forcing
+using Oceananigans.Units
+using Printf
+
+@inline function mean_velocity(x, y, z, t, p)
+    h = √(2 * p.ν * p.t₀)
+    U₀ = p.A * p.t₀
+    δ = z / h
+    return U₀ * ((1 + δ^2) * erfc(-δ / √2) + δ * √(2/π) * exp(-δ^2 / 2))
+end
+
+function decaying_turbulence_on_shear_flow(arch;
+                                           # Cross-stream direction
+                                           Ny = 256,
+                                           Ly = 0.2,
+                                           # Along-stream direction
+                                           Nx = Ny,
+                                           Lx = Ly,
+                                           # Vertical direction
+                                           Nz = round(Int, Ny/2),
+                                           Lz = Ly / 2,
+                                           ν = 1.05e-6,
+                                           β = 1.2e-5,
+                                           initial_time = 0.0,
+                                           stop_time = 10.0,
+                                           save_interval = 0.2,
+                                           overwrite_existing = true,
+                                           name = "initial_turbulence")
+
+    refinement = 1.5 # controls spacing near surface (higher means finer spaced)
+    stretching = 8   # controls rate of stretching at bottom
+    h(k) = (k - 1) / Nz
+    ζ₀(k) = 1 + (h(k) - 1) / refinement
+    Σ(k) = (1 - exp(-stretching * h(k))) / (1 - exp(-stretching))
+
+    @info "Building a grid..." 
+    grid = RectilinearGrid(arch,
+                           size = (Nx, Ny, Nz),
+                           halo = (3, 3, 3),
+                           x = (0, Lx),
+                           y = (0, Ly),
+                           z = k -> Lz * (ζ₀(k) * Σ(k) - 1), # (-Lz, 0)
+                           topology = (Periodic, Periodic, Bounded))
+
+    @show grid
+
+    @show A = β * sqrt(π / 4ν)
+    t₀ = initial_time
+    U = BackgroundField(mean_velocity, parameters=(; A, ν, t₀))
+
+    model = NonhydrostaticModel(; grid,
+                                background_fields = (; u=U),
+                                advection = CenteredSecondOrder(),
+                                #advection = WENO(),
+                                timestepper = :RungeKutta3,
+                                closure = ScalarDiffusivity(; ν))
+
+    # Set initial condition
+    Random.seed!(123)
+    wᵢ(x, y, z) = 10 * A * t₀ * randn()
+    set!(model, u=wᵢ, v=wᵢ, w=wᵢ)
+
+    @info "Revvving up a simulation..."
+    simulation = Simulation(model; Δt=1e-6, stop_time)
+
+    Δ = min(minimum(parent(grid.Δzᵃᵃᶜ)), grid.Δxᶜᵃᵃ)
+    @show max_Δt = 0.1 * Δ^2 / ν
+    wizard = TimeStepWizard(; cfl=0.5, max_Δt)
+    simulation.callbacks[:wizard] = Callback(wizard, IterationInterval(3))
+    wall_clock = Ref(time_ns())
+
+    # Closure over wall_clock
+    function progress(sim)
+        umax = maximum(abs, sim.model.velocities.u)
+        vmax = maximum(abs, sim.model.velocities.v)
+        wmax = maximum(abs, sim.model.velocities.w)
+
+        t = time(sim)
+        elapsed = 1e-9 * (time_ns() - wall_clock[])
+
+        @info @sprintf("Time: %s, iter: %d, Δt: %s, wall time: %s, max|U|: (%.2e, %.2e, %.2e)  m s⁻¹",
+                       prettytime(t),
+                       iteration(sim),
+                       prettytime(sim.Δt),
+                       prettytime(elapsed),
+                       umax, vmax, wmax)
+
+        wall_clock[] = time_ns()
+
+        return nothing
+    end
+
+    simulation.callbacks[:progress] = Callback(progress, IterationInterval(10))
+
+    #####
+    ##### Set up output
+    #####
+
+    Nx, Ny, Nz = size(model.grid)
+
+    file_prefix = @sprintf("%s_beta%d_t0%d_N%d_%d_%d_L%d_%d_%d",
+                           name, 1e7 * β, initial_time,
+                           Nx, Ny, Nz,
+                           100 * model.grid.Lx,
+                           100 * model.grid.Ly,
+                           100 * model.grid.Lz)
+
+    nobackup_dir = "."
+    dir = joinpath(nobackup_dir, file_prefix)
+
+    @info "Saving data to $file_prefix"
+    schedule = SpecifiedTimes(0.1, 0.2, 0.4, 0.8, 1.6)
+    simulation.output_writers[:fields] = JLD2OutputWriter(model, model.velocities; dir,
+                                                          overwrite_existing, schedule,
+                                                          filename = file_prefix * "_fields")
+
+    return simulation
+end
+
+# For example:
+# julia --project initial_turbulence.jl 768 768 512 0.2 0.2 0.1 1.2 16
+#                                       Nx  Ny  Nz  Lx   Ly  Lz   β t₀
+
+#=
+parsing = false
+Nx = Ny = 64
+Nz = 48
+Lx = Ly = 0.2
+Lz = 0.1
+β = 1.2e-5
+t₀ = 16
+
+arch = CPU()
+=#
+
+parsing = true
+arch = GPU()
+
+if parsing
+    Nx     = parse(Int,     ARGS[1])
+    Ny     = parse(Int,     ARGS[2])
+    Nz     = parse(Int,     ARGS[3])
+    Lx     = parse(Float64, ARGS[4])
+    Ly     = parse(Float64, ARGS[5])
+    Lz     = parse(Float64, ARGS[6])
+    β      = parse(Float64, ARGS[7]) * 1e-5
+    t₀     = parse(Float64, ARGS[8])
+end
+
+simulation = decaying_turbulence_on_shear_flow(arch; Nx, Ny, Nz, Lx, Ly, Lz, β, initial_time = t₀)
+run!(simulation)
+
+@info "Simulation complete: $simulation. Output:"
+
+for (name, writer) in simulation.output_writers
+    if !(writer isa Checkpointer)
+        absfilepath = abspath(writer.filepath)
+        @info "OutputWriter $name, $absfilepath:\n $writer"
+    end
+end
+
